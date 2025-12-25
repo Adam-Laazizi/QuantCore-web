76408bd5c34ce12c94681c670a3594e407aad50c
Adam-Laazizi <adam.laazizi06@gmail.com>
2025-12-25 16:30:50 -0500

    Established CNAME file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // This ensures the site loads correctly on GitHub Pages subfolders
-  base: '/QuantCore-web/', 
+  // base: '/QuantCore-web/', 
   plugins: [react()],
 })
